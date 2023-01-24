71fe01b41cc13ddbc541592929ab50b91eb50e1d
trainingsexponent <105920671+trainingsexponent@users.noreply.github.com>
2023-01-24 21:06:50 +0530

    Student Data Set.

--- DemoNew/src/com/Test.java
+++ DemoNew/src/com/Test.java
@@ -5,5 +5,13 @@ public class Test {
 	public static void main(String[] args) {
 		
 		System.out.println("Check Feature Create Class Branch..");
+	
+		Student student = new Student();
+		student.setId(12);
+		student.setName("Abcd");
+		student.setAddress("pune");
+		
+		System.out.println("Check Student Data : " + student);
+	
 	}
 }
